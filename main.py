e726ebd521894e8551b302cdff9a146097e073c1
Harshavardhan Bajoria <hvbajoria@hotmail.com>
2023-11-24 23:42:46 +0530

    Fixed bugs

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from languages import languages
 import logging
 import os
 
-@st.experimental_singleton
+@st.cache_resource
 def installff():
   os.system('sbase install geckodriver')
   os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
